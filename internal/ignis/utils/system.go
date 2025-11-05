9931b6596a2a3936ee916ad1c344a395a70dbbc0
kekwy <keekkewy@qq.com>
2025-11-05 17:28:36 +0800

    refactor: rename packages and files
    - rename packages and files
    - add protobuf files for ignis
    - add python protobuf files for ignis
    - add go protobuf files for ignis
    - add python protobuf files for peer
    - add go protobuf files for peer
    - add python protobuf files for platform
    - add go protobuf files for platform

---
+++ internal/ignis/utils/system.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"github.com/asynkron/protoactor-go/actor"
+	"github.com/lmittmann/tint"
+	"io"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/lithammer/shortuuid/v4"
+)
+
+func WithLogger(logPaths ...string) actor.ConfigOption {
+	writers := []io.Writer{os.Stderr}
+	for _, log := range logPaths {
+		w, err := os.OpenFile(log, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+		writers = append(writers, w)
+	}
+
+	return actor.WithLoggerFactory(func(system *actor.ActorSystem) *slog.Logger {
+		return slog.New(tint.NewHandler(io.MultiWriter(writers...), &tint.Options{
+			AddSource:  true,
+			Level:      slog.LevelDebug,
+			TimeFormat: time.DateTime,
+		})).With("system", system.ID)
+	})
+}
+
+func GenID() string {
+	return shortuuid.New()
+}
+
+func GenIDWith(prefix string) string {
+	return prefix + shortuuid.New()
+}
