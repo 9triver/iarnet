3633609ece25898e9e9c8aed06ca625f6ae9ec40
kekwy <keekkewy@qq.com>
2025-11-13 17:09:02 +0800

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/9triver/iarnet/internal/config"
-	domaincontroller "github.com/9triver/iarnet/internal/domain/ignis/controller"
-	domainresource "github.com/9triver/iarnet/internal/domain/resource"
-	domaincomponent "github.com/9triver/iarnet/internal/domain/resource/component"
-	domainstore "github.com/9triver/iarnet/internal/domain/resource/store"
+	"github.com/9triver/iarnet/internal/domain/ignis/controller"
+	"github.com/9triver/iarnet/internal/domain/resource"
+	"github.com/9triver/iarnet/internal/domain/resource/component"
+	"github.com/9triver/iarnet/internal/domain/resource/store"
 	"github.com/9triver/iarnet/internal/transport/rpc"
 	"github.com/9triver/iarnet/internal/transport/zmq"
 	"github.com/9triver/iarnet/internal/util"
 	"github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
 )
 
 func main() {
@@ -28,7 +25,6 @@ func main() {
 	flag.Parse()
 
 	cfg, err := config.LoadConfig(*configFile)
-
 	if err != nil {
 		log.Fatalf("Load config: %v", err)
 	}
@@ -38,40 +34,25 @@ func main() {
 
 	util.InitLogger()
 
-	lis, err := net.Listen("tcp4", fmt.Sprintf("0.0.0.0:%d", cfg.Ignis.Port))
-	if err != nil {
-		logrus.Fatalf("failed to listen: %v", err)
-	}
-	server := grpc.NewServer()
-
 	channeler := zmq.NewChanneler(cfg.ZMQ.Port)
-	store := domainstore.NewStore()
-	resourceManager := domainresource.NewManager(channeler, store, cfg.ComponentImages)
-	controllerManager := domaincontroller.NewManager(resourceManager)
-	rpc.RegisterIgnisServer(server, controllerManager)
+	store := store.NewStore()
+	resourceManager := resource.NewManager(channeler, store, cfg.ComponentImages)
+	controllerManager := controller.NewManager(resourceManager)
 
-	logrus.Infof("Ignis server listening on %s", lis.Addr().String())
-	go func() {
-		if err := server.Serve(lis); err != nil && !errors.Is(err, grpc.ErrServerStopped) {
-			logrus.Fatalf("failed to serve: %v", err)
-		}
-	}()
-
-	storeLis, err := net.Listen("tcp4", fmt.Sprintf("0.0.0.0:%d", cfg.Resource.Store.Port))
-	if err != nil {
-		logrus.Fatalf("failed to listen: %v", err)
+	ripcAddr := fmt.Sprintf("0.0.0.0:%d", cfg.Ignis.Port)
+	storeAddr := fmt.Sprintf("0.0.0.0:%d", cfg.Resource.Store.Port)
+	if err := rpc.Start(rpc.Options{
+		IgnisAddr:     ripcAddr,
+		StoreAddr:     storeAddr,
+		ControllerMgr: controllerManager,
+		StoreService:  resourceManager,
+	}); err != nil {
+		logrus.Fatalf("failed to start rpc servers: %v", err)
 	}
-	storeServer := grpc.NewServer()
-	rpc.RegisterStoreServiceServer(storeServer, resourceManager)
+	logrus.Infof("Ignis server listening on %s", ripcAddr)
+	logrus.Infof("Store server listening on %s", storeAddr)
 
-	logrus.Infof("Store server listening on %s", storeLis.Addr().String())
-	go func() {
-		if err := storeServer.Serve(storeLis); err != nil && !errors.Is(err, grpc.ErrServerStopped) {
-			logrus.Fatalf("failed to serve store service: %v", err)
-		}
-	}()
-
-	controllerService := domaincontroller.NewService(controllerManager, resourceManager, resourceManager)
+	controllerService := controller.NewService(controllerManager, resourceManager, resourceManager)
 
 	// Create context with cancellation for graceful shutdown
 	ctx, cancel := context.WithCancel(context.Background())
@@ -85,7 +66,7 @@ func main() {
 
 	logrus.Info("Iarnet started successfully")
 
-	provider := domaincomponent.NewProvider("local-docker", "localhost", 50051)
+	provider := component.NewProvider("local-docker", "localhost", 50051)
 	resourceManager.RegisterProvider(provider)
 	logrus.Infof("Local docker provider registered")
 	controllerService.CreateController(context.Background(), "1")
@@ -106,14 +87,8 @@ func main() {
 		logrus.Errorf("Error closing ZMQ channeler: %v", err)
 	}
 
-	server.GracefulStop()
-	storeServer.GracefulStop()
+	rpc.Stop()
 	logrus.Info("gRPC servers stopped")
-
-	// Close listeners to stop accepting new connections
-	lis.Close()
-	storeLis.Close()
-	logrus.Info("Listeners closed")
 
 	logrus.Info("Shutdown complete")
 }
